dcee03b9c7c0c660276a38c2c73402d1a06ec3ff
Marta Jopp <martapeterson@gmail.com>
2018-05-21 22:38:10 -0500

    firebase auth

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = 
+    {
+        apiKey: "AIzaSyDOdi_idHF7hHe3kslNHZH9VKjogFyX6CY",
+        authDomain: "fir-auth-5a029.firebaseapp.com",
+        databaseURL: "https://fir-auth-5a029.firebaseio.com",
+        projectId: "fir-auth-5a029",
+        storageBucket: "fir-auth-5a029.appspot.com",
+        messagingSenderId: "344954610593"
+    };
